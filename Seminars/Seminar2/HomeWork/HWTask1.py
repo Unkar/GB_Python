014b395879269225d05e8110f5c6d73e729c75b4
Sergey <2805026@gmail.com>
2022-10-01 21:06:01 +0300

    Seminar2/HWTask1.py was fixed

--- Seminars/Seminar2/HomeWork/HWTask1.py
+++ Seminars/Seminar2/HomeWork/HWTask1.py
@@ -4,28 +4,44 @@
 
 # - 6782 -> 23
 # - 0,56 -> 11
-def sum_digits(number):
-    sum = 0
+
+def get_fractional_part(number):
+    return number - int(number)
+
+def get_integer_part(number):
+    return int(number)
+
+def get_fractional_count_digits(number):
+    count = 0
+    while number != int(number):
+        number *= 10
+        count += 1
+    return count
+
+def sum_digits(number, count=0):
     if number < 0:
         number = -number
-
-    number_before_dot = round(number)
-    number_after_dot = number - number_before_dot
-
-    while number_before_dot > 0:
-        sum += number_before_dot % 10
-        print(number_before_dot % 10)
-        number_before_dot //= 10
-
-    while number_after_dot != 0:
-        number_after_dot *= 10
-        sum += round(number_after_dot)
-        number_after_dot -= round(number_after_dot)
+    sum = 0
+    if number > 1:
+        while number > 0:
+            sum += number % 10
+            number = number // 10
+    else:
+        for i in range(count):
+            number *= 10
+            sum += int(number % 10)
     return sum
 
 def main():
     number = float(input("Введите число: "))
-    print(sum_digits(number))   
+    fractional_part = get_fractional_part(number)
+    integer_part = get_integer_part(number)
+    print("количество цифр в дробной части: ", get_fractional_count_digits(number))
+    print(f"Целая часть: {integer_part}")
+    print(f"сумма цифр целой части: {sum_digits(integer_part)}")
+    print(f"Дробная часть: {fractional_part}")
+    print(f"сумма цифр дробной части: {sum_digits(fractional_part, get_fractional_count_digits(number))}")
+    # print(sum_digits(number))   
 
 if __name__ == "__main__": #Запуск программы если файл запущен как основной
     main()
