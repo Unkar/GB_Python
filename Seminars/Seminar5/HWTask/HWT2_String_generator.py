497ab5a494a899390f110ffda6f88a4f0e157cd7
Sergey <2805026@gmail.com>
2022-10-07 11:42:44 +0300

    Seminar5/HWTask/HWT2_String_generator.py was added

---
+++ Seminars/Seminar5/HWTask/HWT2_String_generator.py
@@ -0,0 +1,18 @@
+PATH ="Seminars\Seminar5\HWTask\"
+
+def random_string_generator(length):
+    import random
+    import string
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+def save_string_to_file(filename, string):
+    with open(filename, 'w') as f:
+        f.write(string)
+
+def main():
+    string_length = int(input('Введите длину строки: '))
+    filename = PATH + "HWT_input_string.txt"
+    save_string_to_file(filename, random_string_generator(string_length))
+
+if __name__ == '__main__':
+    main()
