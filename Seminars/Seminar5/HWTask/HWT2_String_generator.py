5fe245ff2a1d05e9d634d8799c1f0358c4fb939e
Sergey <2805026@gmail.com>
2022-10-07 13:52:45 +0300

    Seminar5/HWTask/HWT2_String_generator.py was fixed

--- Seminars/Seminar5/HWTask/HWT2_String_generator.py
+++ Seminars/Seminar5/HWTask/HWT2_String_generator.py
@@ -1,10 +1,10 @@
 import random
 import string
 
-PATH = "Seminars/Seminar5/HWTask/"
+PATH = "Seminars/Seminar5/HWTask/HWT2_original_string.txt"
 
 def random_string_generator(quantity_generation):
-    return ''.join(random.choice(string.ascii_letters)*random.randint(1,4) for i in range(quantity_generation))
+    return ''.join(random.choice(string.ascii_letters)*random.randint(1,8) for i in range(quantity_generation))
 
 def save_string_to_file(filename, string):
     with open(filename, 'w') as f:
@@ -14,8 +14,9 @@ def main():
     # Коэффициент генерации строк, сколько раз будет генерироваться символы в строке.
     # Сделан, чтобы были совпадающие символы в строке.
     quantity_generation = int(input('Введите коэффициент генерациии: '))
-    filename = PATH + "HWT_original_string.txt"
+    filename = PATH
     save_string_to_file(filename, random_string_generator(quantity_generation))
+    print("Файл сгенерирован и сохранен в " + filename)
 
 if __name__ == '__main__':
     main()
