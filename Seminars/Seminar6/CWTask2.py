79bb868846930f7d65fde716784dcea36b945237
Sergey <2805026@gmail.com>
2022-10-10 21:47:24 +0300

    Seminar6/CWTask2.py

---
+++ Seminars/Seminar6/CWTask2.py
@@ -0,0 +1,22 @@
+# 41. Напишите программу вычисления арифметического выражения заданного строкой. Используйте операции +,-,/,*. приоритет операций стандартный. 
+    
+#     *Пример:* 
+    
+#     2+2 => 4; 
+    
+#     1+2*3 => 7; 
+    
+#     1-2*3 => -5;
+
+def calc(expression):
+    if expression.find('+') != -1:
+        return calc(expression[:expression.find('+')]) + calc(expression[expression.find('+') + 1:])
+    if expression.find('-') != -1:
+        return calc(expression[:expression.find('-')]) - calc(expression[expression.find('-') + 1:])
+    if expression.find('*') != -1:
+        return calc(expression[:expression.find('*')]) * calc(expression[expression.find('*') + 1:])
+    if expression.find('/') != -1:
+        return calc(expression[:expression.find('/')]) / calc(expression[expression.find('/') + 1:])
+    return int(expression)
+
+print(calc(input()))
