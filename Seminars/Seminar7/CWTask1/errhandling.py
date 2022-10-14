31ca3225500fb0f9453c34226350091c4ea75aa7
Sergey <2805026@gmail.com>
2022-10-14 16:10:59 +0300

    Обработчик ввода пунктов меню заменён на универсальный

--- Seminars/Seminar7/CWTask1/errhandling.py
+++ Seminars/Seminar7/CWTask1/errhandling.py
@@ -1,9 +1,11 @@
-from importlib.abc import Traversable
-
-
-def check_menu_error(value):
-    if value  in ["1", "2", "3"]:
+# Path: errhandling.py
+def check_menu_error(value, max_value):
+    """Обрабатывает ошибку ввода пункта меню.
+Если вводимые данные не соответствуют типу и значению данных, то выводит сообщение об ошибке и возвращает True."""
+    menu_items = [str(i) for i in range(1,max_value+1)]
+    if value  in menu_items:
         return False
     else:
-        print("Ошибка ввода. Повторите ввод.")
+        print("Ошибка ввода. Нет такого пункта в меню. Повторите ввод.")
         return True
+
