20fa39e1b920b73a1e888a5f1097d30ed5156ea7
Sergey <2805026@gmail.com>
2022-10-16 17:39:18 +0300

    Добавлена обработка ошибок и запись в лог

--- Seminars/Seminar7/CWTask1/calculator.py
+++ Seminars/Seminar7/CWTask1/calculator.py
@@ -1,7 +1,8 @@
 """ На функцию подаётся выражение в текстовом виде. Необходимо вернуть результат вычисления. """
+import logger as log
 
-
-def calculate_rational(expression):
+def calculate_rational(expression: str):
+    """ Вычисление рациональных выражении без скобок """
     if expression.find('+') != -1:
         return calculate_rational(expression[:expression.find('+')]) + calculate_rational(expression[expression.find('+')+1:])
     elif expression.find('-') != -1:
@@ -13,7 +14,25 @@ def calculate_rational(expression):
     else:
         return float(expression)
 
-def calculatE(expression):
+def calculate(expression: str):
+    """ Вычисление выражении с учётом скобок """
     while expression.find(')') != -1:
         expression = expression[:expression.rfind('(', 0, expression.find(')'))] + str(calculate_rational(expression[expression.rfind('(', 0, expression.find(')'))+1:expression.find(')')])) + expression[expression.find(')')+1:]
     return calculate_rational(expression)
+
+def main():
+    """ Основная функция """
+    while True:
+        expression = input("Введите выражение (для выхода введите 'q'): ")
+        try:
+            if expression == 'q':
+                break
+            print(expression + ' = ' + str(calculate(expression)))
+            log.write_log_calc(expression, calculate(expression))
+        except Exception as e:
+            print("Ошибка ввода выражения. Повторите ввод. Тип ошибки:" + str(e))
+            log.write_log_error("Ошибка ввода выражения. Повторите ввод. Тип ошибки:" + str(e))
+
+
+if __name__ == '__main__':
+    main()
