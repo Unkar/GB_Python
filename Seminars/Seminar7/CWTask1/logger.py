daeac1145556fe1d10e5086909e0d688d0862bd1
Sergey <2805026@gmail.com>
2022-10-16 17:49:33 +0300

    Изменён формат записи в лог. Добавлена функция считывания лога. Функция возвращает список словарей с ключами date, type, message

--- Seminars/Seminar7/CWTask1/logger.py
+++ Seminars/Seminar7/CWTask1/logger.py
@@ -1,11 +1,35 @@
 import datetime as dt
+import config as cfg
 
-def write_log(expression, result):
-    with open('calculations.txt', 'a') as f:
-        f.write(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S")} : {expression} = {result}')
 
-def read_log(option):
-    if option:
-        pass
-    with open('calculations.txt', 'r') as f:
-        return f.readlines()
+
+def write_log_calc(expression: str, result: float):
+    """ Запись в лог вычислений """
+    if cfg.history_recording_status:
+        with open(cfg.PATH_LOG, 'a', encoding= 'utf-8') as f:
+            f.write(f"""Date :: {dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S")} ==>> Calc :: {expression}={result}\n""")
+
+def write_log_error(error: str):
+    """ Запись в лог ошибок """
+    if cfg.history_recording_status:
+        with open(cfg.PATH_LOG, 'a', encoding= 'utf-8') as f:
+            f.write(f"""Date :: {dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S")} ==>> Error :: {error}\n""")
+
+def write_log_status(status: str):
+    """ Запись в лог статуса """
+    if  cfg.history_recording_status:
+        with open(cfg.PATH_LOG, 'a', encoding= 'utf-8') as f:
+            f.write(f"""Date :: {dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S")} ==>> Status :: {status}\n""")
+
+def read_all_log():
+    """ Вывод всего лога как список словарей с ключами date, type, message """
+    with open(cfg.PATH_LOG, 'r', encoding= 'utf-8') as f:
+        log_list = []
+        for line in f:
+            log_element = {}
+            log_element['date'] = line.split('::')[1].split('==>>')[0].strip()
+            log_element['type'] = line.split('==>>')[1].split('::')[0].strip()
+            log_element['message'] = line.split('==>>')[1].split('::')[1].strip()
+            log_list.append(log_element)
+    return log_list
+
