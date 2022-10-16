377e09b4db1e7060d7b2da2310addda6fad8650e
Sergey <2805026@gmail.com>
2022-10-16 17:42:52 +0300

    Добавлены статусы вкл/выкл записи истории и отображения ошибок и записей изменения статусов при запросе истории. А также добавлены обработчики переключения статусов

--- Seminars/Seminar7/CWTask1/config.py
+++ Seminars/Seminar7/CWTask1/config.py
@@ -1,4 +1,29 @@
-from menu import history_visiable
+import logger as log
+import datetime as dt
 
 
-history_visiable_status = True
+history_recording_status = True
+history_error_visiable_status = True
+history_status_visiable = True
+PATH_LOG = 'log.txt'
+
+def history_recording():
+    global history_recording_status
+    if history_recording_status == True:
+        log.write_log_status("История выключена")
+        print('История выключена')
+    else:
+        log.write_log_status('История включена')
+        print('История включена')
+    history_recording_status = not history_recording_status
+
+def error_visiable():
+    global history_error_visiable_status
+    if history_error_visiable_status == True:
+        history_error_visiable_status = False
+        log.write_log_status("История ошибок выключена")
+        print('История ошибок выключена')
+    else:
+        history_error_visiable_status = True
+        log.write_log_status('История ошибок включена')
+        print('История ошибок включена')
