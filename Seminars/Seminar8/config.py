488ae6d3899e6fb8cd76fb072b50f9a766b01c86
Sergey <2805026@gmail.com>
2022-10-18 23:11:49 +0300

    config.py добавлена структура конфигурации настроек

---
+++ Seminars/Seminar8/config.py
@@ -0,0 +1,13 @@
+#config.json = ['role_config':
+#                { "@role1": { "param1": "value1", "param2": "value2" },
+#                  "@role2": { "param1": "value1", "param2": "value2" },
+#                  "@role3": { "param1": "value1", "param2": "value2" }
+#                },
+#                'user_config':
+#                { "@userID": { "param1": "value1", "param2": "value2" },
+#                  "@userID": { "param1": "value1", "param2": "value2" },
+#                  "@userID": { "param1": "value1", "param2": "value2" }
+#                }
+#              ]
+#
+# config.py - модуль для работы с конфигурационным файлом
